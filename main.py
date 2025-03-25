a94e97867b82f65f47dd724b319c1df251e75986
Hunter <akulkaushal13579@gmail.com>
2025-03-25 19:00:13 +0530

    setup of notion api done

--- main.py
+++ main.py
@@ -1,14 +1,26 @@
 import requests
-from FastAPI import FastAPI, HTTPException
-
-"""
-    loading api key
-"""
+from fastapi import FastAPI, HTTPException  
+import json
 from dotenv import load_dotenv
 import os
+from notion_client import Client  
+
+"""
+    Setting up Notion Client
+"""
+
 
 load_dotenv()
 NOTION_API_KEY = os.getenv("NOTION_API_KEY")
-NOTION_DATABASE_ID= od.getenv("NOTION_DATABASE_ID")
+NOTION_DATABASE_ID = os.getenv("NOTION_DATABASE_ID")  
 
-#checking for update
+
+notion = Client(auth=NOTION_API_KEY) 
+database_id = NOTION_DATABASE_ID
+
+# Fetching Database entry / drop
+try:
+    response = notion.databases.query(database_id=database_id)
+    print(json.dumps(response, indent=4))
+except Exception as e:
+    print("Error:", e)
