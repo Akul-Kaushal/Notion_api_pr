53d35e859a0eaa8078f225811c400b8a51a5cf59
Hunter <akulkaushal13579@gmail.com>
2025-03-25 18:30:47 +0530

    updated some files

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
-import os
 import requests
 from FastAPI import FastAPI, HTTPException
+
+"""
+    loading api key
+"""
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+NOTION_API_KEY = os.getenv("NOTION_API_KEY")
+NOTION_DATABASE_ID= od.getenv("NOTION_DATABASE_ID")
+
